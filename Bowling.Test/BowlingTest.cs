9c1a5da38cafafacaa21a0c3060c408afb71428f
Décamps Laurent <laurent.decamps@diagngrow.com>
2024-04-01 09:40:09 +0200

    refactor: :recycle: refactor test with Theory and Inline data

--- Bowling.Test/BowlingTest.cs
+++ Bowling.Test/BowlingTest.cs
@@ -4,33 +4,16 @@ namespace Bowling.Test;
 
 public class BowlingTest
 {
-    [Fact]
-    public void Make_Roll_of_Zero_Should_Give_A_Score_Of_Zero()
+    [Theory]
+    [InlineData(0)]
+    [InlineData(1)]
+    [InlineData(2)]
+    public void Make_Roll_of_Zero_Should_Give_A_Score_Of_Zero(int numberPins)
     {
         var game = new Game();
 
-        game.Roll(0);
+        game.Roll(numberPins);
 
-        game.Score().Should().Be(0);
-    }
-
-    [Fact]
-    public void Make_Roll_of_One_Should_Give_A_Score_Of_One()
-    {
-        var game = new Game();
-
-        game.Roll(1);
-
-        game.Score().Should().Be(1);
-    }
-
-    [Fact]
-    public void Make_Roll_of_Two_Should_Give_A_Score_Of_Two()
-    {
-        var game = new Game();
-
-        game.Roll(2);
-
-        game.Score().Should().Be(2);
+        game.Score().Should().Be(numberPins);
     }
 }
